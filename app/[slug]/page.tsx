4a4b8efdf4306145cde9f5807132179a95e50f1e
*vivek <vrvivekofficial@gmail.com>
2025-12-27 08:26:49 +0530

    feat: improve blog readability with Tailwind Typography plugin

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,6 +1,6 @@
-import BlogContent from "@/components/BlogContent";
-
 import { fetchPageBySlug } from "@/lib/fetchPage";
+import { cleanHTML } from "@/lib/sanitizeHtml";
+import BlogContent from "@/components/BlogContent";
 
 type Props = {
   params: {
@@ -8,15 +8,15 @@ type Props = {
   };
 };
 
-
 export default async function Page({ params }: Props) {
   const { slug } = await params;
   const page = await fetchPageBySlug(slug);
-  console.log(page);
+  const cleanContent = cleanHTML(page.content.rendered);
 
   return (
-    <main className="max-w-4xl mx-auto px-4 py-10">
+    <main className="max-w-7xl mx-auto px-4 py-10">
       <h1 className="text-4xl font-bold mb-6">{page.title.rendered}</h1>
+      <BlogContent content={cleanContent} />
     </main>
   );
 }
