ba09ceca70e964ca3ea84a7257e63b7c62c7c8a5
*vivek <vrvivekofficial@gmail.com>
2025-12-28 12:21:27 +0530

    style: improve blog content readability and responsive layout

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { fetchPageBySlug } from "@/lib/fetchPage";
 import { cleanHTML } from "@/lib/sanitizeHtml";
 import BlogContent from "@/components/BlogContent";
+import Header from "@/components/Header";
 
 type Props = {
   params: {
@@ -11,12 +12,14 @@ type Props = {
 export default async function Page({ params }: Props) {
   const { slug } = await params;
   const page = await fetchPageBySlug(slug);
+
   const cleanContent = cleanHTML(page.content.rendered);
 
   return (
-    <main className="max-w-7xl mx-auto px-4 py-10">
-      <h1 className="text-4xl font-bold mb-6">{page.title.rendered}</h1>
-      <BlogContent content={cleanContent} />
+    <main className="max-w-7xl mx-auto px-4 ">
+      <div className="mt-20">
+        <BlogContent content={cleanContent} />
+      </div>
     </main>
   );
 }
--- components/BlogContent.tsx
+++ components/BlogContent.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+const blogProse = `
+  prose
+    prose-lg
+    max-w-none
 
+    prose-h1:text-gray-900
+    prose-h1:font-extrabold
+    prose-h1:tracking-tight
+    prose-h1:mb-6
+
+    prose-h2:text-gray-900
+    prose-h2:font-semibold
+    prose-h2:mt-12
+    prose-h2:mb-4
+
+    prose-h3:text-gray-800
+    prose-h3:font-semibold
+
+    prose-p:text-gray-700
+    prose-p:leading-relaxed
+    prose-p:mb-6
+
+    prose-a:text-blue-600
+    prose-a:no-underline
+    hover:prose-a:underline
+
+    prose-ul:pl-6
+    prose-li:marker:text-gray-400
+
+    prose-img:rounded-xl
+    prose-img:shadow-md
+    prose-img:mx-auto
+  
+    prose-img:my-10
+`;
 export default function BlogContent({ content }: { content: string }) {
-  return (
-    <div>
-      <article
-        className="prose prose-lg max-w-none"
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
-  );
+  return <article className={blogProse} dangerouslySetInnerHTML={{ __html: content }} />;
 }
